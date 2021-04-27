18653021c3228513d3672b9d83f0eb75f6ec1cfe
Ramon <81922328+trevinor@users.noreply.github.com>
2021-04-27 12:37:11 -0500

    menu

--- main.cpp
+++ main.cpp
@@ -5,6 +5,27 @@ using namespace std;
 
 int main()
 {
+	int choice;
+	
+	cout << "1 - Client Information\n";
+	cout << "2 - Sales Representatives\n";
+	cout << "3 - Products\n";
+	// cout << "4 - Sales\n";
+	cout << "4 - Exit" << endl << endl;
+	
+	cin >> choice;
+	
+	switch(choice)
+	{
+		case 1:
+			cout << "Client Information: \n";
+			// rest of code here
+		case 2:
+			cout << "Sales Representatives: \n";
+		case 3:
+			
+	}
+	
 	
 	return 0;
 }
