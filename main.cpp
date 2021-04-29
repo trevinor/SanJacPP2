efb9e5a38b4557897b6ccee7e8aa66403ab8f0f8
Ramon <81922328+trevinor@users.noreply.github.com>
2021-04-29 12:00:24 -0500

    Main Menu has been created using switch statements
    I am still modifying this code a little but the layout is there. Functions will need to be placed within each respective casing for each menu. I will be focusing on the Clients first since that is the first on the list.

--- main.cpp
+++ main.cpp
@@ -1,81 +1,203 @@
+// PP2
+// Team Uno
 #include <iostream>
-#include <iomanip>
-#include <fstream>
+
+
 using namespace std;
+
+
 
 int main()
 {
 	int choice;
-	
-	cout << "1 - Client Information\n";
-	cout << "2 - Sales Representatives\n";
-	cout << "3 - Products\n";
-	// cout << "4 - Sales\n";
-	cout << "4 - Exit" << endl << endl;
-	
+
+	// MAIN MENU
+	cout << "---- Welcome to El Habanero's Database System ----" << endl << endl;
+	cout << "MAIN MENU" << endl << endl;
+	cout << "1: Clients" << endl;
+	cout << "2: Sales Representatives" << endl;
+	cout << "3: Products" << endl;
+	cout << "4: Sales" << endl << endl;
+	cout << "Please enter a module: ";
 	cin >> choice;
-	
-	switch(choice)
+
+	switch (choice)
 	{
 		case 1:
-			cout << "Client Information: \n";
-			// rest of code here
+		{
+			// CLIENTS MENU
+			cout << endl << "MAIN MENU > Clients menu" << endl << endl;
+			cout << "1: Read File" << endl;
+			cout << "2: List All" << endl;
+			cout << "3: Search" << endl;
+			cout << "4: Add" << endl;
+			cout << "5: Update" << endl;
+			cout << "6: Save" << endl;
+			cout << "0: Back" << endl;
+
+			while (choice)
+			{
+				cout << endl << "Please enter a module: ";
+				cin >> choice;
+
+				switch (choice)
+				{
+				
+					case 1:
+					{
+						// read clientinformation.txt file function
+						break;
+					}
+					case 2:
+					{
+						// list all data from clientinformation.txt function
+						break;
+					}
+					case 3:
+					{
+						// search client by name from clientinformation.txt function
+						break;
+					}
+					case 4:
+					{
+						// add new client to clientinformation.txt function
+						break;
+					}
+					case 5:
+					{
+						// update client in clientinformation.txt function
+						break;
+					}
+					case 6: 
+					{
+						// save all information function
+						break;
+					}
+					case 0:
+					{
+						// back to main menu function
+						break;
+					}
+				}
+			}
+
+			break;
+		}
 		case 2:
-			cout << "Sales Representatives: \n";
-		case 3:
+		{
+			// SALES REPS MENU
+			cout << endl << "MAIN MENU > Sales Representatives menu" << endl << endl;
+			cout << "1: Read File" << endl;
+			cout << "2: List All" << endl;
+			cout << "3: Search" << endl;
+			cout << "4: Add" << endl;
+			cout << "5: Update" << endl;
+			cout << "6: Save" << endl;
+			cout << "0: Back" << endl;
+
+			while (choice)
+			{
+				cout << endl << "Please enter a module: ";
+				cin >> choice;
+
+				switch (choice)
+				{
+
+					case 1:
+					{
+						// read salesreps.txt file function
+					break;
+					}
+					case 2:
+					{
+						// list all data from salesrep.txt function
+						break;
+					}
+					case 3:
+					{
+						// search sales rep by name from salesrep.txt function
+						break;
+					}
+					case 4:
+					{
+						// add new sales rep to salesrep.txt function
+						break;
+					}
+					case 5:
+					{
+						// update sales rep in salesrep.txt function
+						break;
+					}
+					case 6:
+					{
+						// save all information function
+						break;
+					}
+					case 0:
+					{
+						// back to main menu function
+						break;
+					}
+				}
+			}
 			
+			
+
+			break;
+		}
+		case 3:
+		{
+			// PRODUCTS MENU
+			cout << endl << "MAIN MENU > Products menu" << endl << endl;
+			cout << "1: Read File" << endl;
+			cout << "2: List All" << endl;
+			cout << "3: Add" << endl;
+			cout << "4: Update" << endl;
+			cout << "5: Save" << endl;
+			cout << "0: Back" << endl;
+
+			switch (choice)
+				{
+
+				case 1:
+				{
+					// read products.txt file function
+					break;
+				}
+				case 2:
+				{
+					// list all data from products.txt function
+					break;
+				}
+				case 3:
+				{
+					// add new product to products.txt function
+					break;
+				}
+				case 4:
+				{
+					// update products in product.txt function
+					break;
+				}
+				case 5:
+				{
+					// save all information function
+					break;
+				}
+				case 0:
+				{
+					// back to main menu function
+					break;
+				}
+			}
+		}
+
+			break; // I know something is wrong with the layout of the code right here. If you see a change that would help, let me know.
+		}
 	}
-	
+
+
+
 	
 	return 0;
-}
-//struct Menu;
-    //char printMenu(string inputText) {
-    //char ch;
-    //void showdata();
-    //display menu
-
-    //cout << "\n\nMenu: \n" << "C - Open Client List" << "\nR - Open Sales Representative List" << "\nP - Open Product List" << "\nQ - Quit" << endl;
-    //cout << "\n\nChoose and option: ";
-
-    //cin >> ch;
-
-    //switch(ch) {
-    //case 'c':
-    //case 'C':
-        //cout << "Client list will open here" << endl;
-        //break;
-    //case 'r':
-    //case 'R':
-        //cout << showdata.Print() << endl;
-        //break;
-    //case 'p':
-    //case 'P':
-        //cout << "Product list will open here" << endl;
-        //break;
-    //case 's':
-    //case 'S':
-        //cout << "Sales list will open here" << endl;
-        //break;
-    //case 'q':
-    //case 'Q':
-        //exit (EXIT_SUCCESS);
-
-        //}
-        //return ch;
-//}
-
-
-//int main() {
-    //string inputText, searchWord;
-    //char option;
-
-    //cout << "Welcome to El Habanero Database" << endl;
-
-    //do {
-        //option = printMenu(inputText);
-            //cout << endl;
-   // }
-   // while (option!= 'Q'||'q');
-   // return 0;
 }
