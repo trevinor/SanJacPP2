6bccc8c576648f48d725907026b83034133fbd84
a-felan <77691352+a-felan@users.noreply.github.com>
2021-05-04 09:57:15 -0500

    Update ClientInformation.h

--- ClientInformation.h
+++ ClientInformation.h
@@ -1,11 +1,24 @@
-#inndef ClientInformation_H
-#define ClientInformation_H
+#ifndef CLIENT INFO_H
+#define CLIENT INFO_H
+
 
 class ClientInformation
 {
-  public:
-  
-  private:
+    public:
+
+        string getClientInfo () {return c_specinfo;}
+        void AddClient(string &cName, string &cAddress, string &cPhoneNum, string &cSalesToDate);
+        void ClientInfo();
+
+    private:
+        string cName;
+//      string cMiddleName;
+//      string CLastName;
+        string cAddress;
+        string cPhoneNum;
+        string cSalesToDate;
+        string newClient;
+        string c_specinfo;
 };
 
-#endif
+#endif // CLIENT INFO_H
